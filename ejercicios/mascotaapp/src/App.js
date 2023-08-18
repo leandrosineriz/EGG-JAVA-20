93c86f7e7b3160c702b54f7b233220fd39a2323e
leandrosineriz <leandrosineriz@gmail.com>
2023-08-18 17:35:57 -0300

    hola

---
+++ ejercicios/mascotaapp/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import { Footer } from './components/public/Footer';
+import { Navbar } from './components/public/Navbar';
+import Main from './components/public/Main';
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Main />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ ejercicios/mascotaapp/src/components/public/Footer.js
@@ -0,0 +1,43 @@
+import React, { useState } from 'react'
+
+export const Footer = () => {
+
+    const [clicks, setClicks] = useState(0);
+
+    const year = new Date().getFullYear();
+    const companyName = "EGG Education";
+
+    const handleClick = () => {
+        console.log("Holis");
+        setClicks(clicks+1);
+    }
+    
+    return (
+        <div>
+            <div className="container">
+                <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+                <p className="col-md-4 mb-0 text-body-secondary">© { year } {companyName} Clicks={clicks}</p>
+                <span
+                    to={"/"}
+                    onClick={handleClick}
+                >
+                    <img
+                    className='App-logo'
+                    src='logo192.png'
+                    alt='logo'
+                    >
+                    </img>
+                </span>
+                <ul className="nav col-md-4 justify-content-end">
+                    <li className="nav-item"><a href="#" className="nav-link px-2 text-body-secondary">Home</a></li>
+                    <li className="nav-item"><a href="#" className="nav-link px-2 text-body-secondary">Features</a></li>
+                    <li className="nav-item"><a href="#" className="nav-link px-2 text-body-secondary">Pricing</a></li>
+                    <li className="nav-item"><a href="#" className="nav-link px-2 text-body-secondary">FAQs</a></li>
+                    <li className="nav-item"><a href="#" className="nav-link px-2 text-body-secondary">About</a></li>
+                </ul>
+                </footer>
+            </div>
+        </div>
+    )
+ 
+}
